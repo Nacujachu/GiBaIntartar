b6fea6f76ffb2ff39d5a04602466d5abc32fd21e
傅子睿 <ray@RayiMac.local>
2017-05-14 15:35:29 +0800

    Add LoginActivity, MainMenuActivity

---
+++ FinBot/app/src/main/java/nthu/finbot/LoginActivity.java
@@ -0,0 +1,19 @@
+package nthu.finbot;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void pressLoginButton(View v) {
+        startActivity(new Intent(this, MainMenuActivity.class));
+    }
+}
