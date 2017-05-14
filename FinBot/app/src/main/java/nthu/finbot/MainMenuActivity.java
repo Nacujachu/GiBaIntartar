7ed53aeb8b6dcb8545b16a26052c8e1e26944fd0
傅子睿 <ray@RayiMac.local>
2017-05-14 16:32:12 +0800

    Edit MainMenuActiviy PopupNews

--- FinBot/app/src/main/java/nthu/finbot/MainMenuActivity.java
+++ FinBot/app/src/main/java/nthu/finbot/MainMenuActivity.java
@@ -1,7 +1,10 @@
 package nthu.finbot;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 
 public class MainMenuActivity extends AppCompatActivity {
@@ -30,5 +33,12 @@ public class MainMenuActivity extends AppCompatActivity {
 
     public void pressOpenReservationButton(View v) {
         System.out.println("Press OpenReservation");
+    }
+
+    public void pressOpenNewsButton(View v) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(MainMenuActivity.this);
+        alert.setView(LayoutInflater.from(MainMenuActivity.this).inflate(R.layout.popup_news, null));
+
+        alert.show();
     }
 }
