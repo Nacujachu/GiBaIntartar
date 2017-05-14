d3e4201b2c38f9dbd541c7167485e5006b916416
傅子睿 <ray@RayiMac.local>
2017-05-14 16:11:31 +0800

    Edit MainMenuActivity

--- FinBot/app/src/main/java/nthu/finbot/MainMenuActivity.java
+++ FinBot/app/src/main/java/nthu/finbot/MainMenuActivity.java
@@ -2,6 +2,7 @@ package nthu.finbot;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -9,5 +10,25 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+
+    public void pressBarButton(View v) {
+        System.out.println("Press Bar");
+    }
+
+    public void pressOpenBotButton(View v) {
+        System.out.println("Press OpenBot");
+    }
+
+    public void pressOpenPropertyButton(View v) {
+        System.out.println("Press OpenProperty");
+    }
+
+    public void pressOpenQAButton(View v) {
+        System.out.println("Press OpenQA");
+    }
+
+    public void pressOpenReservationButton(View v) {
+        System.out.println("Press OpenReservation");
     }
 }
